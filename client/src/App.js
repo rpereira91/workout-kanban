123cc40721a6ae371b3cf16760a5e6267f919041
Ralph Pereira <rp09rl@brocku.ca>
2021-03-07 17:53:40 -0500

    added new exercise page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import BoardDisplay from './components/BoardDisplay';
+import AddExercise from './components/AddExercise';
 import { Provider } from 'react-redux'
 import store from './redux/store';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import './App.css';
 
@@ -8,7 +10,14 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <BoardDisplay />
+        <Router>
+            <Switch>
+              <Route exact path='/add' component={AddExercise} />
+              <Route path='/' component={BoardDisplay} />
+              <Route render={() => <Redirect to='/' />} />
+
+            </Switch>
+        </Router>
       </div>
     </Provider>
   );
