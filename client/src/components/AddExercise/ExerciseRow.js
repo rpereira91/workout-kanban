18b5ca37aef5c5c06397f1830420f488ff2914c9
rpereira91 <boveah@gmail.com>
2021-04-06 21:14:37 -0400

    adding exercises

--- client/src/components/AddExercise/ExerciseRow.js
+++ client/src/components/AddExercise/ExerciseRow.js
@@ -7,15 +7,21 @@ import {map} from 'lodash';
 import {EXERCISE_TYPES_SELECT, EQUIPMENT, EXERCISE_TYPES} from '../../constants/constant'
 
 const ExerciseRow = ({exercise, onChange, position, allTags}) => {
-    const {title, tags, type} = exercise
+    const {title, tags, type, equipment} = exercise
     const renderType = () => {
         switch (type) {
             case EXERCISE_TYPES.SET_REP:
                 return (
                     <div>Sets and reps</div>
                 )
-                break;
-        
+            case EXERCISE_TYPES.INTERVAL:
+                return (
+                    <div>Total Seconds</div>
+                )
+            case EXERCISE_TYPES.TIMER:
+                return (
+                    <div>Total Seconds</div>
+                )
             default:
                 return null
         }
@@ -26,25 +32,34 @@ const ExerciseRow = ({exercise, onChange, position, allTags}) => {
     return (
         <div className="exerciseRow">
             <Input value={title} placeholder="Exercise Name" onChange={({target: {value}}) => onChange(position, 'title',value )}/>
-            <Select 
-                isMulti
-                closeMenuOnSelect={false}
-                options={getSelect(allTags)}
-                onChange={(options) => onChange(position, 'tags', map(options, (tag) => tag.label))}
-                label="Tags"
-                style={{width: `15em`}}    
-            />
-            <Select
-                options={EXERCISE_TYPES_SELECT}
-                onChange={(selected) => onChange(position, 'type', selected.value)}
-                label="Type"
-            />
-            <Select
-                isMulti
-                options={getSelect(EQUIPMENT)}
-                onChange={(options) => onChange(position, 'equipment', map(options, (tag) => tag.label))}
-                label="Equipment"
-            />
+            <div className="selectContainer">
+                <Select 
+                    isMulti
+                    closeMenuOnSelect={false}
+                    options={getSelect(allTags)}
+                    onChange={(options) => onChange(position, 'tags', map(options, (tag) => tag.label))}
+                    label="Tags"
+                    style={{width: `15em`}}    
+                    value={getSelect(tags)}
+                />
+            </div>
+            <div className="selectContainer">
+                <Select
+                    options={EXERCISE_TYPES_SELECT}
+                    onChange={(selected) => onChange(position, 'type', selected.value)}
+                    label="Type"
+                    valye={EXERCISE_TYPES_SELECT[type]}
+                />
+            </div>
+            <div className="selectContainer">
+                <Select
+                    isMulti
+                    options={getSelect(EQUIPMENT)}
+                    onChange={(options) => onChange(position, 'equipment', map(options, (tag) => tag.label))}
+                    label="Equipment"
+                    value={getSelect(equipment)}
+                />
+            </div>
             {renderType()}
         </div>
     )
--- client/src/components/AddExercise/index.js
+++ client/src/components/AddExercise/index.js
@@ -3,31 +3,24 @@ import {Button, Input } from '@material-ui/core';
 import ExerciseRow from './ExerciseRow';
 import {connect} from 'react-redux'
 import * as boardActions from '../../redux/WorkoutBoard/actions';
-import { map } from 'lodash';
+import { map, isEmpty, isNil } from 'lodash';
 import {EXERCISE_TYPES} from '../../constants/constant'
 import './AddExercise.css'
 // import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 const TEMP_EXERCISE = {
-    title: 'Kettlebell Squat',
+    title:null,
     type:EXERCISE_TYPES.SET_REP,
-    tags: ['Legs', ],
+    tags: [],
     default_reps: 10,
     default_sets: 10,
-    equipment: ['Kettlebell', 'Dumbell'],
-    notes: ['Moved from Do Today to To Do on 04/03/2021 at 2:12pm'],
+    equipment: [],
+    notes: [],
     column: 0,
 }
 const AddExercise = ({history, addExercise, tags}) => {
-    // const [newExercises, setNewExercises] = useState([{
-    //     exercise_name: '',
-    //     tags: [],
-    //     equipment: [],
-    //     notes: [],
-    //     column: 0,
-    //   },])
+
     const [newExercises, setNewExercises] = useState([TEMP_EXERCISE])
     const submitAddExercise = async () => {
-
         addExercise(newExercises, history.push('/'))
     }
     const onExerciseChange = (index, key, value) => {
@@ -35,7 +28,13 @@ const AddExercise = ({history, addExercise, tags}) => {
         editedExercise[index][key] = value
         setNewExercises([...editedExercise])
     }
-    console.log(newExercises)
+    const canSubmitExercises = () => {
+        map (newExercises, (exercise, index) => {
+            for (const [key, value] of Object.entries(exercise)) {
+                console.log(key, value, (isEmpty(value) || isNil(value)), index)
+              }
+        })
+    }
     return (
         <div>
             Add exercise
@@ -45,7 +44,7 @@ const AddExercise = ({history, addExercise, tags}) => {
             {
                 map(newExercises, (exercise, index) => <ExerciseRow allTags={tags} exercise={exercise} onChange={onExerciseChange} position={index}/>)
             }
-            <Button onClick={submitAddExercise}>Add exercise</Button>
+            <Button disabled={canSubmitExercises()} onClick={submitAddExercise}>Add exercise</Button>
             </div>
         </div>
     );
